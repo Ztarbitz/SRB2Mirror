972590e52da7108eba5f7759e92160b9447daa0d
Ztarbitz <aquapoyobuko@gmail.com>
2026-01-01 16:15:09 -0500

    ehe

---
+++ L_VivisPrettyPalettesV2.lua
@@ -0,0 +1,309 @@
+freeslot(
+"SKINCOLOR_AQUA_UNDERGLOW",
+"SKINCOLOR_BEACHED",
+"SKINCOLOR_BIRDHOUSE",
+"SKINCOLOR_BLUH",
+"SKINCOLOR_CAR_WASH",
+"SKINCOLOR_CODE_RED",
+"SKINCOLOR_CRYSTAL_PURPLE",
+"SKINCOLOR_CUSTARD",
+"SKINCOLOR_DARK_CHOCOLATE",
+"SKINCOLOR_DARKISH_BLUE",
+"SKINCOLOR_DECK_OF_CARDS",
+"SKINCOLOR_DYING_SUNSET",
+"SKINCOLOR_EUPHORIA",
+"SKINCOLOR_FUNKY_RAINBOW",
+"SKINCOLOR_GUH",
+"SKINCOLOR_HOT_HEADED",
+"SKINCOLOR_LEMON_OUT_OF_HELL",
+"SKINCOLOR_LIMEISH",
+"SKINCOLOR_METALIC_GOLD",
+"SKINCOLOR_MOLD_EN_MONO",
+"SKINCOLOR_NEUTRAL",
+"SKINCOLOR_OLDY_COLDY",
+"SKINCOLOR_LUKEWARM_PINK",
+"SKINCOLOR_PINKISH_OCEAN",
+"SKINCOLOR_PRINCE_OF_DARKNESS",
+"SKINCOLOR_SNOWED",
+"SKINCOLOR_STARTING_LINE",
+"SKINCOLOR_SWAMPED",
+"SKINCOLOR_TOWERED_WHITE",
+"SKINCOLOR_TROPHY",
+"SKINCOLOR_UNBALANCED_RAINBOW",
+"SKINCOLOR_WALLPAPER",
+"SKINCOLOR_WATERED",
+"SKINCOLOR_WELLDWELLER"
+)
+
+skincolors[SKINCOLOR_AQUA_UNDERGLOW] = {
+	name = "Aqua Underglow",
+	ramp = {31,30,139,139,138,138,143,137,137,142,136,141,140,140,3,0},
+	invcolor = SKINCOLOR_AQUA_UNDERGLOW,
+	invshade = 0,
+	chatcolor = V_BLUEMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_BEACHED] = {
+	name = "Beached",
+	ramp = {0,48,50,52,86,91,141,141,142,142,137,138,138,139,29,31},
+	invcolor = SKINCOLOR_BEACHED,
+	invshade = 0,
+	chatcolor = V_ORANGEMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_BIRDHOUSE] = {
+	name = "Birdhouse",
+	ramp = {73,72,64,89,89,90,92,93,105,106,107,108,109,109,109,110},
+	invcolor = SKINCOLOR_BIRDHOUSE,
+	invshade = 0,
+	chatcolor = V_REDMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_BLUH] = {
+	name = "bluh.",
+	ramp = {0,128,120,121,121,121,121,141,141,135,136,137,138,138,138,138},
+	invcolor = SKINCOLOR_BLUH,
+	invshade = 0,
+	chatcolor = V_AQUAMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_CAR_WASH] = {
+	name = "Car Wash",
+	ramp = {85,85,8,7,7,7,140,131,132,171,194,183,184,184,185,207},
+	invcolor = SKINCOLOR_CAR_WASH,
+	invshade = 0,
+	chatcolor = V_AQUAMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_CODE_RED] = {
+	name = "Code Red",
+	ramp = {31,47,46,44,42,41,39,38,36,35,37,39,42,45,47,31},
+	invcolor = SKINCOLOR_CODE_RED,
+	invshade = 0,
+	chatcolor = V_REDMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_CRYSTAL_PURPLE] = {
+	name = "Crystal Purple",
+	ramp = {164,164,163,163,163,162,162,161,161,161,160,160,176,144,1,0},
+	invcolor = SKINCOLOR_CRYSTAL_PURPLE,
+	invshade = 0,
+	chatcolor = V_PURPLEMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_CUSTARD] = {
+	name = "Custard",
+	ramp = {80,80,80,80,80,81,81,82,83,83,72,72,72,72,72,72},
+	invcolor = SKINCOLOR_CUSTARD,
+	invshade = 0,
+	chatcolor = V_YELLOWMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_DARK_CHOCOLATE] = {
+	name = "Dark Chocolate",
+	ramp = {69,69,69,233,233,79,79,236,236,237,239,239,29,111,30,31},
+	invcolor = SKINCOLOR_DARK_CHOCOLATE,
+	invshade = 0,
+	chatcolor = V_BROWNMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_DARKISH_BLUE] = {
+	name = "Darkish Blue",
+	ramp = {134,134,134,171,172,172,172,173,173,173,173,173,197,197,196,196},
+	invcolor = SKINCOLOR_DARKISH_BLUE,
+	invshade = 0,
+	chatcolor = V_BLUEMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_DECK_OF_CARDS] = {
+	name = "Deck of Cards",
+	ramp = {31,28,24,21,17,13,9,5,5,7,192,193,193,194,194,184},
+	invcolor = SKINCOLOR_DECK_OF_CARDS,
+	invshade = 0,
+	chatcolor = V_PURPLEMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_DYING_SUNSET] = {
+	name = "Dying Sunset",
+	ramp = {176,180,182,182,183,203,203,215,215,60,62,63,62,69,68,77},
+	invcolor = SKINCOLOR_DYING_SUNSET,
+	invshade = 0,
+	chatcolor = V_ROSYMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_EUPHORIA] = {
+	name = "Euphoria",
+	ramp = {129,129,128,1,0,1,252,252,176,200,200,200,201,201,201,201},
+	invcolor = SKINCOLOR_EUPHORIA,
+	invshade = 0,
+	chatcolor = V_SKYMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_FUNKY_RAINBOW] = {
+	name = "Funky Rainbow",
+	ramp = {178,201,218,64,84,90,9,140,133,133,147,162,179,179,201,202},
+	invcolor = SKINCOLOR_FUNKY_RAINBOW,
+	invshade = 0,
+	chatcolor = V_REDMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_GUH] = {
+	name = "guh.",
+	ramp = {0,48,49,51,52,52,52,221,212,203,203,203,194,205,206,185},
+	invcolor = SKINCOLOR_GUH,
+	invshade = 0,
+	chatcolor = V_ORANGEMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_HOT_HEADED] = {
+	name = "Hot Headed",
+	ramp = {177,201,202,202,203,203,33,34,34,34,36,38,39,40,41,42},
+	invcolor = SKINCOLOR_HOT_HEADED,
+	invshade = 0,
+	chatcolor = V_REDMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_LEMON_OUT_OF_HELL] = {
+	name = "Lemon Out of Hell",
+	ramp = {74,73,73,73,74,64,50,219,32,33,34,37,215,215,215,233},
+	invcolor = SKINCOLOR_LEMON_OUT_OF_HELL,
+	invshade = 0,
+	chatcolor = V_YELLOWMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_LIMEISH] = {
+	name = "Limeish",
+	ramp = {0,88,89,89,96,96,98,100,103,103,104,105,116,117,117,117},
+	invcolor = SKINCOLOR_LIMEISH,
+	invshade = 0,
+	chatcolor = V_PERIDOTMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_METALIC_GOLD] = {
+	name = "Metalic Gold",
+	ramp = {73,76,78,238,30,28,27,25,23,248,246,244,243,91,86,90},
+	invcolor = SKINCOLOR_METALIC_GOLD,
+	invshade = 0,
+	chatcolor = V_YELLOWMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_MOLD_EN_MONO] = {
+	name = "Mold En Mono",
+	ramp = {34,34,33,32,32,213,214,215,61,63,71,47,46,187,198,186},
+	invcolor = SKINCOLOR_MOLD_EN_MONO,
+	invshade = 0,
+	chatcolor = V_REDMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_NEUTRAL] = {
+	name = "Neutral",
+	ramp = {0,82,73,74,65,220,211,192,192,163,163,195,196,198,199,31},
+	invcolor = SKINCOLOR_NEUTRAL,
+	invshade = 0,
+	chatcolor = V_PURPLEMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_OLDY_COLDY] = {
+	name = "Oldy Coldy",
+	ramp = {80,80,80,80,80,3,120,129,129,140,148,135,172,173,174,175},
+	invcolor = SKINCOLOR_OLDY_COLDY,
+	invshade = 0,
+	chatcolor = V_SKYMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_LUKEWARM_PINK] = {
+	name = "Lukewarm Pink",
+	ramp = {182,183,183,183,184,205,205,206,207,207,207,43,44,45,71,46},
+	invcolor = SKINCOLOR_LUKEWARM_PINK,
+	invshade = 0,
+	chatcolor = V_MAGENTAMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_PINKISH_OCEAN] = {
+	name = "Pinkish Ocean",
+	ramp = {210,211,211,193,194,194,195,172,172,172,172,173,173,167,168,186},
+	invcolor = SKINCOLOR_PINKISH_OCEAN,
+	invshade = 0,
+	chatcolor = V_BROWNMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_PRINCE_OF_DARKNESS] = {
+	name = "Prince of Darkness",
+	ramp = {123,124,125,105,23,26,28,30,28,186,206,204,203,202,202,201},
+	invcolor = SKINCOLOR_PRINCE_OF_DARKNESS,
+	invshade = 0,
+	chatcolor = V_ROSYMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_SNOWED] = {
+	name = "Snowed",
+	ramp = {0,0,0,1,1,128,120,120,120,140,140,140,141,141,142,126},
+	invcolor = SKINCOLOR_SNOWED,
+	invshade = 0,
+	chatcolor = V_SKYMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_STARTING_LINE] = {
+	name = "Starting Line",
+	ramp = {52,224,68,232,236,29,31,29,139,139,127,127,143,142,142,141},
+	invcolor = SKINCOLOR_STARTING_LINE,
+	invshade = 0,
+	chatcolor = V_ORANGEMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_SWAMPED] = {
+	name = "Swamped",
+	ramp = {73,74,75,189,190,190,191,191,104,105,105,106,117,117,117,117},
+	invcolor = SKINCOLOR_SWAMPED,
+	invshade = 0,
+	chatcolor = V_PERIDOTMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_TOWERED_WHITE] = {
+	name = "Towered White",
+	ramp = {0,2,4,7,170,170,171,172,172,136,172,172,172,149,163,163},
+	invcolor = SKINCOLOR_WHITE,
+	invshade = 0,
+	chatcolor = V_PURPLEMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_TROPHY] = {
+	name = "Trophy",
+	ramp = {65,65,65,86,86,241,12,13,14,243,244,245,246,231,232,78},
+	invcolor = SKINCOLOR_TROPHY,
+	invshade = 0,
+	chatcolor = V_BROWNMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_UNBALANCED_RAINBOW] = {
+	name = "Unbalanced Rainbow",
+	ramp = {210,220,52,52,65,74,74,188,97,112,112,123,132,134,135,136},
+	invcolor = SKINCOLOR_UNBALANCED_RAINBOW,
+	invshade = 0,
+	chatcolor = V_ORANGEMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_WALLPAPER] = {
+	name = "Wallpaper",
+	ramp = {48,48,216,85,240,240,241,242,87,244,229,231,233,234,236,237},
+	invcolor = SKINCOLOR_WALLPAPER,
+	invshade = 0,
+	chatcolor = V_BROWNMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_WATERED] = {
+	name = "Watered",
+	ramp = {120,120,140,170,171,171,149,172,172,172,172,172,172,173,186,187},
+	invcolor = SKINCOLOR_WATERED,
+	invshade = 0,
+	chatcolor = V_BLUEMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_WELLDWELLER] = {
+	name = "Welldweller",
+	ramp = {0,0,0,0,208,209,32,33,34,36,43,30,31,31,31,31},
+	invcolor = SKINCOLOR_WELLDWELLER,
+	invshade = 0,
+	chatcolor = V_REDMAP,
+	accessible = true
+}
